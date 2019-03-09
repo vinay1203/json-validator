a712f98d871a43f91466e48039b7b986adac3b24
vinay1203 <srinivasab@zeta.tech>
2019-03-09 07:52:05 +0530

    for clear json format return

--- mainFile.py
+++ mainFile.py
@@ -1,7 +1,8 @@
 from flask import Flask 
 from flask import render_template,request,flash
 from flask import redirect, url_for
-from flask import json
+from flask import jsonify
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='e5ac358c-f0bf-11e5-9e39-d3b532c10a28'
@@ -15,15 +16,16 @@ def index():
 #Need to validate and json the data that is taken from the textarea..
 @app.route("/jsonify_data", methods=["POST"])
 def jsonify_data():
-	json_val=request.form['json_txt']
+	json_val=json.loads(request.form['json_txt'])
 	try:
-		json_object=json.loads(json_val)
-		return render_template('json.html',json_object=json_object)
+		return jsonify(json_val)
+#		json_object=json.loads(json_val)
+#		json_object=json.dumps(json_object, indent=3)
+#		return render_template('json.html',json_object=json_object)
 	except ValueError:
-		flash("Looks like not a valid json text")
 		return redirect(url_for('index'))
-	json_object=json.loads('json_val')
-	return render_template('json.html', json_object=json_object)
+#	json_object=json.loads('json_val')
+#	return render_template('json.html', json_object=json_object)
 
 if __name__=="__main__":
 	app.run(host="localhost", port=5004, debug=True)
